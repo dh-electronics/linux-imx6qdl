95b0392b1b24ffffc03ab90828d3f7c72342bffe
Christoph Niedermaier <cniedermaier@dh-electronics.com>
2021-09-30 15:20:52 +0200

    drm/panel: simple/lvds: Add call of function drm_mode_show_timings_pref_mode()
    Upstream-Status: Inappropriate [It's only a debug output]
    
    Signed-off-by: Christoph Niedermaier <cniedermaier@dh-electronics.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -420,6 +420,8 @@ static int panel_simple_get_modes(struct drm_panel *panel,
 	 */
 	drm_connector_set_panel_orientation(connector, p->orientation);
 
+	drm_mode_show_timings_pref_mode(connector);
+
 	return num;
 }
 
